f7a75e1cbe24fe7b03e2968f556e64bf8043b588
Mariano <marianobata1@gmail.com>
2024-03-01 10:51:34 -0300

    landing page

--- index2.php
+++ index2.php
@@ -1,27 +1,115 @@
-<?php
-include './assets/template/header.php';
-?>
-<title>Lic. Marina Claros</title>
-<main>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Marina Claros Nutricionista</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="assets/css/estilo_landing.css">
+</head>
+
 <body>
-    <header>
+<nav class="navbar navbar-expand-lg">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="assets/img/logo_nutricion.jpeg" alt="Logo" style="width: 100px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#sobre_mi">Sobre mi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#servicios">Servicios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#testimonios">Testimonios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contacto">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-md-8" id="sobre_mi">
+      <h1>Bienvenido a la Consulta de Nutrición</h1>
+      <p>Estoy acá para ayudarte a transformar tu vida a través de la alimentación y el estilo de vida saludables.
+          ¿Estás listo para tomar el control de tu bienestar y descubrir el poder que la nutrición puede tener en tu vida? Permíteme guiarte en este viaje hacia una mejor salud y felicidad.
+          Creo firmemente que la nutrición es la base de un estilo de vida saludable y feliz. Con el enfoque adecuado en la alimentación equilibrada y sostenible, puedes lograr cambios significativos en tu energía, estado de ánimo y bienestar general.
+          Mi objetivo es trabajar contigo para desarrollar un plan personalizado que se adapte a tus necesidades individuales, metas y estilo de vida. No se trata de dietas restrictivas o contar calorías, sino de aprender a tomar decisiones alimenticias saludables y sostenibles que te ayuden a sentirte mejor cada día.
+          Ya sea que estés buscando perder peso, aumentar tu energía, mejorar tu rendimiento deportivo o simplemente adoptar hábitos alimenticios más saludables, estoy aquí para brindarte el apoyo y la orientación que necesitas para alcanzar tus objetivos.
+          Juntos, podemos trabajar para crear un cambio positivo y duradero en tu vida. ¿Estás listo para comenzar este emocionante viaje hacia una versión más saludable y feliz de ti mismo? ¡Contáctame hoy mismo y demos el primer paso hacia una nueva y vibrante tú!</p>
+        <a href="#contacto" class="btn btn-primary">Agenda una consulta</a>
+    </div>
+    <div class="col-md-4">
+        <img src="assets/img/marina_claros.png" class="img-fluid">
+    </div>
+  </div>
+  <br>
+    
+
+    <section id="servicios" class="section section-bg" style="background-image: url('assets/img/img101.jpg');">
+      <div class="bg-overlay"></div>
       <div class="container">
-        <div class="navbar">
-          <div class="logo">
-            <img src="assets/img/logo_nutricion.jpeg" alt="" />
-          </div>
-          <nav>
-            <div class="btn">
-              <i class="fa fa-times close-btn"></i>
-            </div>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
-          </nav>
-          <div class="btn">
-            <i class="fa fa-bars menu-btn"></i>
-          </div>
+        <h2 class="section-text">Servicios</h2>
+        <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo massa nec felis sagittis, sit amet dictum justo volutpat. Integer finibus velit ac quam condimentum, vel efficitur ligula tempus. Sed condimentum, quam at interdum vehicula, lectus lectus sodales justo, nec posuere orci turpis sit amet nulla.</p>
+      </div>
+    </section>
+    
+
+    <section id="testimonios" class="section section-bg" style="background-image: url('assets/img/testimonios.jpg');">
+      <div class="bg-overlay"></div>
+      <div class="container">
+        <h2 class="section-text">Testimonios</h2>
+        <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo massa nec felis sagittis, sit amet dictum justo volutpat. Integer finibus velit ac quam condimentum, vel efficitur ligula tempus. Sed condimentum, quam at interdum vehicula, lectus lectus sodales justo, nec posuere orci turpis sit amet nulla.</p>
+      </div>
+    </section>
+    
+    <section id="contacto" class="section section-bg" style="background-image: url('assets/img/contacto.jpg');">
+      <div class="bg-overlay"></div>
+      <div class="container">
+        <h2 class="section-text">Contacto</h2>
+        <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo massa nec felis sagittis, sit amet dictum justo volutpat. Integer finibus velit ac quam condimentum, vel efficitur ligula tempus. Sed condimentum, quam at interdum vehicula, lectus lectus sodales justo, nec posuere orci turpis sit amet nulla.</p>
+      </div>
+    </section>
+  </div>
+
+  <a href="https://wa.me/1137921389" class="whatsapp-btn" target="_blank" rel="noopener noreferrer">
+    <i class="fab fa-whatsapp"></i>
+  </a>
+
+  <footer class="bg-light text-center py-4">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h5>Sígueme en las redes sociales</h5>
+        <div class="social-icons">
+          <a href="#" class="social-icon"><i class="fab fa-facebook"></i></a>
+          <a href="#" class="social-icon"><i class="fab fa-twitter"></i></a>
+          <a href="#" class="social-icon"><i class="fab fa-instagram"></i></a>
+          <a href="#" class="social-icon"><i class="fab fa-linkedin"></i></a>
         </div>
       </div>
-    </header>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <p>© 2024 Marina Claros. Todos los derechos reservados.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+  <!-- jQuery, Popper.js, Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
