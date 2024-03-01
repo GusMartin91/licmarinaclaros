89e3c40f367d699ea20e0bed71856c29c5774137
Mariano <marianobata1@gmail.com>
2024-03-01 11:07:52 -0300

    color hamburguesa

--- index2.php
+++ index2.php
@@ -5,8 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Marina Claros Nutricionista</title>
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="assets/bootstrap-5.3.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/estilo_landing.css">
 </head>
 
@@ -20,7 +20,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto">
+      <ul class="navbar-nav ms-auto">
         <li class="nav-item">
           <a class="nav-link" href="#sobre_mi">Sobre mi</a>
         </li>
@@ -107,9 +107,7 @@
     </div>
   </div>
 </footer>
-  <!-- jQuery, Popper.js, Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="assets/dataTables/datatables.min.js"></script>
+  <script src="assets/bootstrap-5.3.0/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
